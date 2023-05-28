e8bc3059064ca27b05b01d05c4bf1f54b145b054
arraniri46 <garda.arraniri@gmail.com>
2023-05-28 11:42:02 +0700

    ready for testing phase

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,51 +1,51 @@
-const btnSubmit = document.getElementById("submit-button");
-const formAppointment = document.getElementById("form-appointment");
+const btnSubmit = document.getElementById('submit-button')
+const formAppointment = document.getElementById('form-appointment')
 
-btnSubmit.addEventListener("click", (e) => {
-  e.preventDefault();
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault()
 
-  //   let dataAppointment = {};
+    //   let dataAppointment = {};
 
-  const nameValue = formAppointment.elements["name"].value;
-  const phoneValue = formAppointment.elements["phone"].value;
-  const emailValue = formAppointment.elements["email"].value;
-  const dateValue = formAppointment.elements["date"].value;
-  const timeValue = formAppointment.elements["time"].value;
-  const messageValue = formAppointment.elements["message"].value;
+    const nameValue = formAppointment.elements['name'].value
+    const phoneValue = formAppointment.elements['phone'].value
+    const emailValue = formAppointment.elements['email'].value
+    const dateValue = formAppointment.elements['date'].value
+    const timeValue = formAppointment.elements['time'].value
+    const messageValue = formAppointment.elements['message'].value
 
-  //   dataAppointment = {
-  //     nameValue,
-  //     phoneValue,
-  //     emailValue,
-  //     dateValue,
-  //     timeValue,
-  //     messageValue,
-  //   };
+    //   dataAppointment = {
+    //     nameValue,
+    //     phoneValue,
+    //     emailValue,
+    //     dateValue,
+    //     timeValue,
+    //     messageValue,
+    //   };
 
-  //   console.log(dataAppointment);
+    //   console.log(dataAppointment);
 
-  const fromEmail = "gardaatwork@gmail.com";
-  const fromName = "The Healing Loft";
-  const formattedFrom = `${fromName} <${fromEmail}>`;
+    const fromEmail = 'gardaatwork@gmail.com'
+    const fromName = 'The Healing Loft'
+    const formattedFrom = `${fromName} <${fromEmail}>`
 
-  if (
-    nameValue === "" ||
-    phoneValue === "" ||
-    emailValue === "" ||
-    dateValue === "" ||
-    timeValue === "" ||
-    messageValue === ""
-  ) {
-    alert("Fill the form please");
-  } else {
-    Email.send({
-      Host: "smtp.elasticemail.com",
-      Username: "gardaatwork@gmail.com",
-      Password: "E23AF35E6F82E23B9B0D5B65A5565DB5777C",
-      To: "gardarevs@gmail.com",
-      From: formattedFrom,
-      Subject: "New From Appointment - The Healing Loft",
-      Body: `
+    if (
+        nameValue === '' ||
+        phoneValue === '' ||
+        emailValue === '' ||
+        dateValue === '' ||
+        timeValue === '' ||
+        messageValue === ''
+    ) {
+        alert('Fill the form please')
+    } else {
+        Email.send({
+            Host: 'smtp.elasticemail.com',
+            Username: 'gardaatwork@gmail.com',
+            Password: 'E23AF35E6F82E23B9B0D5B65A5565DB5777C',
+            To: 'gardarevs@gmail.com',
+            From: formattedFrom,
+            Subject: 'New From Appointment - The Healing Loft',
+            Body: `
         Hi, New appointment is here.<br><br>
         Name: ${nameValue} <br>
         Phone Number: ${phoneValue} <br>
@@ -55,6 +55,6 @@ btnSubmit.addEventListener("click", (e) => {
         Message: ${messageValue} <br><br>
         Thank You
         `,
-    }).then((message) => alert(message + ", Appointment Sent Successfully"));
-  }
-});
+        }).then((message) => alert(message + ', Appointment Sent Successfully'))
+    }
+})
