26b4e1f3354d71eeb7c4ed3db7b7e55e2b089ff4
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2025-11-11 10:31:10 +0700

    refactor: index.tsx to match package version

--- src/components/galleries/index.tsx
+++ src/components/galleries/index.tsx
@@ -11,51 +11,53 @@ import FsLightbox from 'fslightbox-react'
 import { ArrowsPointingOutIcon } from '@heroicons/react/24/solid'
 
 export default function Galleries(): JSX.Element {
-    const [toggler, setToggler] = useState<boolean>(false)
-    const [imageIndex, setImageIndex] = useState<number>(0)
+  const [toggler, setToggler] = useState<boolean>(false)
+  const [imageIndex, setImageIndex] = useState<number>(0)
 
-    const galleries = [
-        Gallery2.src,
-        Gallery3.src,
-        Gallery1.src,
-        Gallery6.src,
-        Gallery4.src,
-        Gallery7.src,
-        Gallery8.src,
-        Gallery5.src,
-    ]
+  const galleries = [
+    Gallery2.src,
+    Gallery3.src,
+    Gallery1.src,
+    Gallery6.src,
+    Gallery4.src,
+    Gallery7.src,
+    Gallery8.src,
+    Gallery5.src,
+  ]
 
-    return (
-        <div>
-            <div className='columns-2 space-y-4 lg:columns-4'>
-                {galleries.map((gallery: any, index: number) => (
-                    <div key={index} className='group relative'>
-                        <div
-                            className='absolute inset-0 flex cursor-pointer items-center justify-center rounded-2xl duration-200 hover:bg-white/60'
-                            onClick={() => {
-                                setToggler(!toggler)
-                                setImageIndex(index)
-                            }}
-                        >
-                            <ArrowsPointingOutIcon className='w-8 text-primary opacity-0 duration-200 group-hover:opacity-100' />
-                        </div>
-
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img
-                            className='break-inside-avoid rounded-2xl hover:cursor-pointer'
-                            src={gallery}
-                            alt='interior'
-                            loading='lazy'
-                        />
-                    </div>
-                ))}
+  return (
+    <div>
+      <div className="columns-2 space-y-4 lg:columns-4">
+        {galleries.map((gallery: string, index: number) => (
+          <div key={index} className="group relative">
+            <div
+              className="absolute inset-0 flex cursor-pointer items-center justify-center rounded-2xl duration-200 hover:bg-white/60"
+              onClick={() => {
+                setToggler(!toggler)
+                setImageIndex(index)
+              }}
+            >
+              <ArrowsPointingOutIcon className="w-8 text-primary opacity-0 duration-200 group-hover:opacity-100" />
             </div>
-            <FsLightbox
-                toggler={toggler}
-                sources={galleries}
-                sourceIndex={imageIndex}
-                types={...new Array(galleries.length).fill('image')}
+
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              className="break-inside-avoid rounded-2xl hover:cursor-pointer"
+              src={gallery}
+              alt="interior"
+              loading="lazy"
             />
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+
+      <FsLightbox
+        toggler={toggler}
+        sources={galleries}
+        sourceIndex={imageIndex}
+        // ini yang tadinya error
+        types={new Array(galleries.length).fill('image')}
+      />
+    </div>
+  )
 }
